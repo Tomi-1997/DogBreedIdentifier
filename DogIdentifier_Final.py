a4d60e7316649362a6b763eb79585d9f165c69c9
Tomi <74303735+Tomi-1997@users.noreply.github.com>
2023-01-07 23:29:04 +0200

    Update DogIdentifier_Final.py

--- DogIdentifier_Final.py
+++ DogIdentifier_Final.py
@@ -1,7 +1,3 @@
-CHANGES = 'Added droput'
-
-## TODO : Class weights
-
 import time
 import pandas as pd # open csv
 import numpy as np
@@ -14,15 +10,15 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split ## Split data to train \ validate
 
 ## Model imports
-from keras.models import Sequential  # initial NN
-from keras.layers import Dense, Dropout, SpatialDropout2D, GlobalAveragePooling2D  # construct each layer
-from keras.layers import Convolution2D # swipe across the image by 1
-from keras.layers import MaxPooling2D  # swipe across by pool size
-from keras.layers import ZeroPadding2D
-from keras.layers import Flatten
-from keras.losses import categorical_crossentropy
-from keras.layers import RandomRotation, RandomFlip, RandomZoom
+from keras.models import Sequential
 from keras import regularizers
+from keras.layers import Dense, Dropout, SpatialDropout2D, GlobalAveragePooling2D, ReLU
+from keras.layers import Convolution2D as Conv2D, LeakyReLU # swipe across the image by 1
+from keras.layers import MaxPooling2D, BatchNormalization
+from keras.losses import categorical_crossentropy
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
 
 DIR = 'C:\\Users\\tomto\\PycharmProjects\\RandomStuff\\dog-breed-identification\\'
 TRAIN_DIR = DIR + '\\train\\'
@@ -30,22 +26,25 @@ TRAIN_DIR = DIR + '\\train\\'
 DATA = pd.read_csv(DIR + '\\labels.csv')
 BREEDS = pd.unique(DATA['breed'])
 BREEDS_NUM = len(BREEDS)
+IMAGES = len(DATA)
 
 
+##### Initialize data #####
 DATA_NAMES = [fname for fname in DATA['id']]   ## All filenames
 DATA_LABEL = [breed for breed in DATA['breed']] ## Breed to true/false vector matching their breed index in breeds list.
 DATA_LABELS = [label == np.array(BREEDS) for label in DATA_LABEL] ## Apply to all.
 
-## Values to resize each image to. (Should be low to take less ram)
-IMG_HEIGHT = 100
+IMG_HEIGHT = 64
 IMG_WIDTH = IMG_HEIGHT
 CHANNELS = 3
 
-BATCH_SIZE = 32
-EPOCH_NUM = 50
+BATCH_SIZE = 16
+EPOCH_NUM = 200
 
-## Can be set to lower number for debugging
-IMAGES = len(DATA)
+##### For each breed, assign a weight #####
+num_of_each_breed = DATA['breed'].value_counts()
+max_breed = max(num_of_each_breed)
+class_weights = {i : (max_breed / num_of_each_breed[i]) for i in range(BREEDS_NUM)}
 
 def process_image(image_path):
     """
@@ -67,76 +66,77 @@ def process_image(image_path):
 
     return img
 
-
-def get_image_label(image_id, label):
-    """
-    Takes an image file name and the associated label,
-    processes the image and returns a tuple of (image, label).
-    """
-    image_path = name_to_path(image_id)
-    image = process_image(image_path)
-    return image, label
-
-
 def name_to_path(fname : str):
     return TRAIN_DIR + fname + '.jpg'
 
-
+# Filepath -> Open image -> To tensor
 DATA_VALUE = [process_image(name_to_path(fname)) for fname in DATA_NAMES]
 
+def guess_my_pics(model):
+    for pic in os.listdir(DIR + "my_pics"):
+        path = DIR + "my_pics\\" + pic
+        test_img = [process_image(path)]
+        res = model.predict(tf.convert_to_tensor(test_img))
+        y_classes = res.argmax(axis=-1)
+        print(f'Actual:{pic.replace(".jpg","")}, Guess:{BREEDS[y_classes]}')
 
-if __name__ == '__main__':
-    # Split data into training & validation
-    X_train, X_val, Y_train, Y_val = train_test_split(DATA_VALUE[:IMAGES],
-                                                      DATA_LABELS[:IMAGES],
-                                                      test_size=0.2, random_state=2)
-
-    X_train = tf.convert_to_tensor(X_train)
-    X_val = tf.convert_to_tensor(X_val)
-    Y_train = tf.convert_to_tensor(Y_train)
-    Y_val = tf.convert_to_tensor(Y_val)
-
+def get_model():
+    activ = ReLU()
+    reg = regularizers.l2(0.001)
+    drop_rate = 0.25
+    filter_size = 8
     ## Sequential : Single inupt --> Single output (Image -> breed)
-    model = Sequential(
-        [
-            RandomRotation(factor=0.2),
-            RandomFlip(),
-            RandomZoom(height_factor=(-0.2, 0.2))
-        ]
-    )
+    model = Sequential()
+    weight_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=4)
+    bias_init = tf.keras.initializers.Zeros()
+    ######################################### Layers
+    model.add(Conv2D(filter_size, kernel_size=(3, 3), strides=(1, 1), padding='same', kernel_initializer=weight_init,
+                     kernel_regularizer=reg, bias_initializer=bias_init, input_shape=(IMG_HEIGHT, IMG_WIDTH, CHANNELS)))
+    model.add(BatchNormalization())
+    model.add(activ)
+    model.add(MaxPooling2D(pool_size=2, padding='same'))
+    model.add(SpatialDropout2D(drop_rate))
 
-    reg = regularizers.l2(0.0001)
-    activ = 'relu'
-
-    ######################################### Model
-    ## Condense information (conv2d).
-    model.add(Convolution2D(filters = 32, kernel_size = (3), activation = activ,
-                            kernel_regularizer = reg, input_shape = (IMG_HEIGHT,IMG_WIDTH,CHANNELS)))
-    model.add(Convolution2D(64, (3, 3), activation = activ, kernel_regularizer = reg))
-    model.add(Convolution2D(128, (3, 3), activation = activ, kernel_regularizer = reg))
-    model.add(SpatialDropout2D(0.5)) ## Dropout certain pixels by groups
-    model.add(GlobalAveragePooling2D()) ## Instead of Flatten()
-    model.add(Dense(units = BREEDS_NUM, activation = 'softmax'))
-    ######################################### Model
-
-    model.compile(loss = categorical_crossentropy, optimizer = 'adam', metrics = ['accuracy'])
-    history = model.fit(X_train, Y_train,
-                        shuffle = True,
-                        batch_size = BATCH_SIZE,
-                        epochs = EPOCH_NUM,
-                        verbose = 2, ## Print info
-                        validation_data = (X_val, Y_val),
-                        initial_epoch = 0)
+    while filter_size < 128:
+        filter_size *= 2
+        model.add(Conv2D(filter_size, kernel_size=(3, 3), strides=(1, 1), padding='same', kernel_initializer=weight_init,
+                         kernel_regularizer=reg, bias_initializer=bias_init))
+        model.add(BatchNormalization())
+        model.add(activ)
+        model.add(MaxPooling2D(pool_size=2, padding='same'))
+        model.add(SpatialDropout2D(drop_rate))
 
 
-    print(CHANGES)
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(1200, activation=activ, kernel_initializer=weight_init,
+                     kernel_regularizer=reg, bias_initializer=bias_init))
 
+    model.add(Dropout(drop_rate))
+
+    model.add(Dense(BREEDS_NUM, activation="softmax"))
+    return model
+
+def get_image_gen():
+    return ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # dimesion reduction
+        rotation_range=40,  # randomly rotate images in the range 40 degrees
+        zoom_range=0.2,  # Randomly zoom image 20%
+        width_shift_range=0.2,  # randomly shift images horizontally 20%
+        height_shift_range=0.2,  # randomly shift images vertically 20%
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+def plot_history(history):
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 2, 1)
     ax2 = fig.add_subplot(1, 2, 2)
 
-    ax1.plot(history.history['val_accuracy'], label='validation') ## Val
-    ax1.plot(history.history['accuracy'], label='train')          ## Train
+    ax1.plot(history.history['val_accuracy'], label='validation')  ## Val
+    ax1.plot(history.history['accuracy'], label='train')  ## Train
 
     ax2.plot(history.history['val_loss'], label='validation')
     ax2.plot(history.history['loss'], label='train')
@@ -150,4 +150,57 @@ if __name__ == '__main__':
 
     plt.show()
     plt.show()
-    model.summary()
+
+def train_model(model, callbacks, opt):
+    model.compile(loss = categorical_crossentropy,
+                  optimizer = opt,
+                  metrics = ['accuracy'])
+    return model.fit(datagen.flow(X_train, Y_train,
+                           batch_size=BATCH_SIZE),
+              shuffle=True,
+              epochs=EPOCH_NUM,
+              verbose=2,  ## Print info
+              validation_data=(X_val, Y_val)
+              , callbacks=callbacks
+              , class_weight=class_weights)
+
+if __name__ == '__main__':
+    # Split data into training & validation
+    X_train, X_val, Y_train, Y_val = train_test_split(DATA_VALUE[:IMAGES],
+                                                      DATA_LABELS[:IMAGES],
+                                                      test_size=0.2, random_state=2)
+
+    X_train = tf.convert_to_tensor(X_train)
+    X_val = tf.convert_to_tensor(X_val)
+    Y_train = tf.convert_to_tensor(Y_train)
+    Y_val = tf.convert_to_tensor(Y_val)
+
+    Train = True
+    checkpoint_path = "training_final/cp.ckpt"
+    model = get_model()
+
+    if Train:
+        datagen = get_image_gen()
+
+        # if validation accuracy doesnt improve for 15 epoch, stop training
+        early_stopping = EarlyStopping(monitor='val_accuracy', patience=15)
+        checkpoint_dir = os.path.dirname(checkpoint_path)
+        checkpointer = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_best_only=True, save_weights_only=True)
+        datagen.fit(X_train)
+        callbacks = [checkpointer, early_stopping]
+        learning_rate = 0.001
+        while True:
+        ###################################################### TRAIN
+            print(f'Starting training with learning of {learning_rate}')
+            history = train_model(model, callbacks, Adam(learning_rate=learning_rate))
+            learning_rate /= 10
+
+    #     model.summary()
+    #     plot_history(history)
+    else:
+        # Try to load earlier model
+        try:
+            model = tf.keras.models.load_model(checkpoint_path)
+            guess_my_pics(model)
+        except Exception:
+            print(f'Could not load previous model.')
